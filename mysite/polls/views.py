bbc5446307977c5c319b9917fc5da9ee387efb9b
Tegan Broderick <teganbroderick@gmail.com>
2020-02-02 15:55:20 -0800

    Added comment about how site is working

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,3 +16,14 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+"""
+When somebody requests a page from your website – say, “/polls/34/”, 
+Django will load the mysite.urls Python module because it’s pointed to 
+by the ROOT_URLCONF setting. It finds the variable named urlpatterns and 
+traverses the patterns in order. After finding the match at 'polls/', it 
+strips off the matching text ("polls/") and sends the remaining text – "34/" – 
+to the ‘polls.urls’ URLconf for further processing. There it matches 
+'<int:question_id>/', resulting in a call to the detail() view like so:
+detail(request=<HttpRequest object>, question_id=34)
+"""
