249e039d64bf28cfe5adbec0ca3f27204a1481af
Tegan Broderick <teganbroderick@gmail.com>
2020-02-05 14:20:52 -0800

    Added comments, troubleshooting index page load

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from djang.urls import reverse
+from django.urls import reverse
 from .models import Choice, Question
 
 def index(request):
